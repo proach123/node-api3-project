58044ac612645358d69be7900b889232fdf41996
Patrick Roach <patrickevanroach@gmail.com>
2020-02-13 16:35:55 -0700

    database problem

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const userRouter = require('./users/userRouter')
 
 const server = express();
+
+server.use("/api/users", logger, userRouter);
+server.use("/api/users/:id", logger, userRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +12,10 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} Request to ${req.originalUrl} `);
+
+  next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,22 +1,72 @@
 const express = require('express');
-
+const db = require('./userDb')
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post("/", (req, res) => {
+
+  db.insert(req.body)
+    .then(db => {
+      res.status(201).json(db);
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: "Error adding the db",
+      });
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post("/:id/posts", (req, res) => {
+    const { id } = req.params;
+  
+    const post = { ...req.body, user_id: id };
+  
+    db.insert(post)
+      .then(inserted => {
+        res.status(201).json(inserted);
+      })
+      .catch(error => {
+        console.log(error);
+  
+        res.status(500).json({ errorMessage: "sorry, we failed you" });
+      });
+  });
+
+router.get("/", (req, res) => {
+  console.log("headers", req.headers); //<<<<<<<<<<<<<<<<<
+
+  db.get(req.query)
+    .then(db => {
+      res.status(200).json(db);
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the db",
+      });
+    });
 });
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get("/:id", (req, res) => {
+  db.getById(req.params.id)
+    .then(db => {
+      if (db) {
+        res.status(200).json(db);
+      } else {
+        res.status(404).json({ message: "db not found" });
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the db",
+      });
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
-});
 
 router.get('/:id/posts', (req, res) => {
   // do your magic!
